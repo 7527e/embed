621944ef906e5af30dcc3635fbe1170146084da5
7527e <aayushsrivastava.iit72@gmail.com>
2022-07-14 17:11:41 +0530

    2 files modified

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ import {
 // Initialize embed configuration
 init({
   thoughtSpotHost:
-    /*param-start-hosturl*/ 'http://localhost:3000/' /*param-end-hosturl*/,
-  authType: AuthType.None,
+    /*param-start-hosturl*/
+    // 'https://172.19.174.234:8443/',
+  'http://localhost:3000/',
+  /*param-end-hosturl*/ authType: AuthType.None,
   username: 'tsadmin',
 });
 
@@ -27,6 +29,7 @@ const embed = new LiveboardEmbed('#embed', {
   disabledActions: [Action.ShowUnderlyingData],
   liveboardId:
     '33248a57-cc70-4e39-9199-fb5092283381' /*param-end-liveboardFullHeight*/ /*param-end-modifyActions*/ /*param-end-liveboardId*/ /*param-end-runtimeFilters*/,
+  liveboardV2: true,
   /*param-start-liveboardFullHeight*/
   /*param-start-modifyActions*/
   /*param-start-liveboardId*/
@@ -36,5 +39,15 @@ const embed = new LiveboardEmbed('#embed', {
 embed.render();
 
 document.getElementById('button1').addEventListener('click', () => {
-  embed.trigger('pin', {vizId: '730496d6-6903-4601-937e-2c691821af3c'});
+  embed.trigger(HostEvent.CopyLink);
+});
+
+
+document.getElementById('button2').addEventListener('click', () => {
+  embed.trigger(HostEvent.CopyLink, {vizId: 'c89fda22-329f-492c-a027-2256cc3e6933'});
+});
+
+
+document.getElementById('button3').addEventListener('click', () => {
+  embed.trigger(HostEvent.CopyLink, {vizId: 'c89fda22-32cdfsd9f-492c-a027-2256cc3e6933'});
 });
